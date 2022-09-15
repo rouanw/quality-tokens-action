367482a81d9b19200752356ec1b846162323eae8
Rouan Wilsenach <rouanw@users.noreply.github.com>
2022-09-15 20:54:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const context = github.context;
+core.info(context.head_commit.message);
 core.info(JSON.stringify(context, null, 2));
